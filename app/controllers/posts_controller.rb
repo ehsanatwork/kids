9f44a26cf1a8eef5251e988e3808fd5cf1faf00e
ehsanatwork <ehsanatwork@gmail.com>
2019-03-17 23:44:01 +0600

    add require_post_user authentication and some styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :required_post_user!, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order("created_at desc")
@@ -47,5 +48,12 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def required_post_user!
+    unless current_user.id == @post.user_id 
+      redirect_to post_path(@post)
+      flash[:danger] = "You are not authorized to perform this action!"
+    end
   end
 end
