ec7258a52f0b3700c3ce1da23435a5c2d55d8119
ehsanatwork <ehsanatwork@gmail.com>
2019-03-22 15:05:23 +0600

    add active storage and thumbnail to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[])
+    params.require(:post).permit(:title, :content, :thumbnail, category_ids:[])
   end
 
   def set_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_one_attached :thumbnail
   has_many :CategoryPost
   has_many :categories, through: :CategoryPost
   belongs_to :user
